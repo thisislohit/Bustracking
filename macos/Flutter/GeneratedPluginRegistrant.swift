ee8cac4326c966703661aa8e55a40e492931a2bc
thisislohit <96004825+thisislohit@users.noreply.github.com>
2023-02-01 21:43:43 +0530

    commit

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,8 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import flutter_secure_storage_macos
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
